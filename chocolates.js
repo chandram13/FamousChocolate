f8f9f795f185358c092f002c7d6e812d3a21a589
Marvish Subhas Chandra <mschandr@uci.edu>
2022-10-17 09:29:41 -0700

    Add files via upload

---
+++ chocolates.js
@@ -0,0 +1,36 @@
+// Marvish Chandra
+
+class chocolates{
+    function chocolateVarieties(){
+        limitedTime = ["Dark Coffee Scotchmallow","Milk Caramel Apple Scotchmallow","Dark Peanut Crunch"];
+        regular = ["Milk Almond Square","Milk Walnuts","Milk Almnond Caramel","Milk Pecan Bud","Dark Walnuts","Dark Walnut Square","Cashew Brittle","Milk Almonds","Milk Wayfair","Milk Peanut Crunch","Milk Peanuts","Milk Rum Nougat","Dark Pecan Bud","Milk Almond Bud","Dark Almond Nougat","Dark California Brittle","Vanilla Nut Caramel","Dark Maple Walnut","Polar Bear Paw","Milk Divinity","Milk Chocolate Brittle","Dark Peanuts","Milk Peanut Nougat","California Crunch","Dark Normandie","Dark Almonds","Milk Strawberry Cream","Milk Chocolate Buttercream","Milk Butterscotch Square","Milk Butterchew","Milk Double Caramel","Milk Peanut Butter Pattie","Cafe Hazelnut Truffle","Milk Molasses Chip","Milk Mocha","Milk Caramel Pattie","Milk Bordeaux","Milk Orange Cream","Milk Coconut Cream","Light Chocolate Truffle","Milk Vanilla Buttercream","Lemon Truffle","Dark Chocolate Buttercream","Dark Caramel Pattie","Deep Dark Chocolate Truffle","Dark Raspberry Cream","Blueberry Truffle","Dark Vanilla Buttercream","Dark Peanut Butter Heart","Dark Salted Caramel","Dark Molasses Chip","Dark Marzipan","Dark Butterchew","Dark Coconut Special","Cafe Au Lait Truffle","Dark Bordeaux","Dark Butterscotch Square","Raspberry Truffle","Dark Scotchmallow","Dark Coconut Cream","Dark Chcoolate Chip Truffle","Apple Pie Truffle","Birthday Cake","Kona Mocha","Apricot Delight","Key Lime Truffle"];
+        rating = ["1 Star", "2 Star","3 Star","4 Star","5 Star"];
+        CustomMix = {"1 lb": 32.50, "2lb": 64.00, "3lb": 93.50, "5lb": 156.50};
+        CustomMixBoxing = ["Lavender Checkerboard Gift Box","White Checkerboard Gift Box"];
+    }
+    function famous_oldtime(){
+        assortedChocolates = {"1lb": 27.50, "2lb": 53.00, "3lb": 78.00, "5lb": 131.00};
+        softCenters = {"1lb": 27.50, "2lb": 53.00};
+        milkChocolates = {"1lb": 27.50, "2lb": 53.00, "3lb": 78.00};
+        nutsandchews = {"1lb": 27.50, "2lb": 53.00, "3lb": 78.00};
+        chocolatevariety = {"1lb": 27.50, "2lb": 53.00};
+        darkChocolates = {"1lb": 27.50, "2lb": 53.00};
+        milkchocolate_nutschews = {"1lb": 32.50};
+        darkChocolate_nutschews = {"1lb": 32.50};
+        milkchocolate_softcenters = {"1lb": 32.50};
+        darkchocolate_softcenters = {"1lb": 32.50};
+        milkBordeaux = {"1lb": 32.50};
+        darkBordeaux = {"1lb": 32.50};
+        darkScotchmallow = {"1lb": 32.50};
+        dark_raspberrycream = {"1lb": 32.50};
+        milkpbpattie = {"1lb": 32.50};
+        milkvanilla_buttercream = {"1lb": 32.50};
+        milkpecanbud = {"1lb": 32.50};
+        darkpecanbud = {"1lb": 32.50};
+        polarbearpaw = {"1lb": 32.50};
+        milkpeanuts = {"1lb": 32.50};
+        milkcaliforniabrittle = {"1lb": 32.50};
+        milkbutterchew = {"1lb": 32.50};
+        milk_butterscotchsquare = {"1lb": 32.50};
+    }
+}
